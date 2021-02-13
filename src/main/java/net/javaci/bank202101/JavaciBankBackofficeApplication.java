8dccf2847bd314a81639500f115b70d86e1f9e7d
volkanistek <volkanistek@gmail.com>
2021-02-13 22:00:21 +0300

    Java version update

--- src/main/java/net/javaci/bank202101/JavaciBankBackofficeApplication.java
+++ src/main/java/net/javaci/bank202101/JavaciBankBackofficeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaciBankBackofficeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaciBankBackofficeApplication.class, args);
+		SpringApplication.run(JavaciBankBackofficeApplication.class, args); 
 	}
 
 }
